86703ba29b92973dcbcc884c14ccdd5f37625a5d
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-11-22 03:15:05 +0100

    wip

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,14 +1,28 @@
 import { Button } from "ui";
 
 import modeles from "@socialgouv/publicodes-demo-modeles";
+import Engine from "publicodes";
+
+import type { Names } from "@socialgouv/publicodes-demo-modeles/dist/orientation-covid";
 
 //console.log(" Names", orientationCovid);
 
-console.log("modeles", modeles);
+//console.log("modeles", modeles);
 export default function Web() {
+  // @ts-ignore TODO
+  const rules = modeles.imc;
+  console.log("rules", rules);
+  const engine = new Engine(rules);
+  engine.setSituation({
+    "patient . poids": "42kg",
+    "patient . taille": "167cm",
+  });
+  const evaluated = engine.evaluate("résultat");
+  console.log("evaluated", evaluated);
+
   return (
     <div>
-      <h1>Web</h1>
+      <h1>IMC: {evaluated.nodeValue}</h1>
       <Button />
     </div>
   );
