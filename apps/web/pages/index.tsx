5759b979c75d33ac7d258def4d4caa603ed4ec9d
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-11-23 02:08:55 +0100

    fixes

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, useState } from "react";
 import { Button } from "ui";
 
 import modeles from "@socialgouv/publicodes-demo-modeles";
@@ -5,25 +6,122 @@ import Engine from "publicodes";
 
 import type { Names } from "@socialgouv/publicodes-demo-modeles/dist/orientation-covid";
 
-//console.log(" Names", orientationCovid);
-
-//console.log("modeles", modeles);
-export default function Web() {
+function IMC() {
+  const [situation, setSituation] = useState({});
+  const [initialMissingVariables, setInitialMissingVariables] = useState<
+    null | string[]
+  >(null);
   // @ts-ignore TODO
   const rules = modeles.imc;
+
   console.log("rules", rules);
   const engine = new Engine(rules);
-  engine.setSituation({
-    "patient . poids": "42kg",
-    "patient . taille": "167cm",
-  });
+  engine.setSituation(situation);
   const evaluated = engine.evaluate("résultat");
   console.log("evaluated", evaluated);
+  const missingVariables = Object.entries(evaluated.missingVariables);
+  const onInputChange = (inputKey: string) => (e: ChangeEvent) => {
+    //@ts-ignore
+    setSituation({ ...situation, [inputKey]: e.currentTarget.value });
+  };
+  if (initialMissingVariables === null && missingVariables.length) {
+    setInitialMissingVariables(
+      Object.values(missingVariables.map(([key]) => key))
+    );
+  }
+  const getQuestion = (key: string) => {
+    const rule = rules[key];
+    console.log("rule", rule);
+    return (rule && rule.question) || null;
+  };
 
   return (
     <div>
-      <h1>IMC: {evaluated.nodeValue}</h1>
-      <Button />
+      <h1>IMC</h1>
+      {(initialMissingVariables &&
+        initialMissingVariables.length &&
+        initialMissingVariables.map((key) => (
+          <div key={key}>
+            {getQuestion(key)}
+            <br />
+            <input
+              type="text"
+              style={{ textAlign: "center" }}
+              onChange={onInputChange(key)}
+            />
+            <hr />
+          </div>
+        ))) ||
+        null}
+      <h3>Résultat : {evaluated.nodeValue}</h3>
+    </div>
+  );
+}
+
+function Orientation() {
+  const [situation, setSituation] = useState({});
+  const [initialMissingVariables, setInitialMissingVariables] = useState<
+    null | string[]
+  >(null);
+  // @ts-ignore TODO
+  const rules = modeles.orientationCovid;
+
+  console.log("rules", rules);
+  const engine = new Engine(rules);
+  engine.setSituation(situation);
+  const evaluated = engine.evaluate("résultat");
+  console.log("evaluated", evaluated);
+  const missingVariables = Object.entries(evaluated.missingVariables);
+  console.log("missingVariables", missingVariables);
+
+  const onInputChange = (inputKey: string) => (e: ChangeEvent) => {
+    const value =
+      //@ts-ignore
+      (isNaN(e.currentTarget.value) && `'${e.currentTarget.value}'`) ||
+      //@ts-ignore
+      e.currentTarget.value;
+
+    setSituation({ ...situation, [inputKey]: value });
+  };
+  if (initialMissingVariables === null && missingVariables.length) {
+    setInitialMissingVariables(
+      Object.values(missingVariables.map(([key]) => key))
+    );
+  }
+  const getQuestion = (key: string) => {
+    const rule = rules[key];
+    console.log("rule", rule);
+    return (rule && rule.question) || null;
+  };
+
+  return (
+    <div>
+      <h1>Orientation COVID</h1>
+      {(initialMissingVariables &&
+        initialMissingVariables.length &&
+        initialMissingVariables.map((key) => (
+          <div key={key}>
+            {getQuestion(key)}
+            <br />
+            <input
+              type="text"
+              style={{ textAlign: "center" }}
+              onChange={onInputChange(key)}
+            />
+            <hr />
+          </div>
+        ))) ||
+        null}
+      <h3>Résultat : {evaluated.nodeValue}</h3>
+    </div>
+  );
+}
+
+export default function Web() {
+  return (
+    <div>
+      <IMC />
+      <Orientation />
     </div>
   );
 }
