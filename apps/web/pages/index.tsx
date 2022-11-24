da33719db819c62a90fd8a3fb63e1ccc38a44dce
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-11-24 17:20:28 +0100

    fix

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,7 +3,7 @@ export default function Web() {
     <div>
       <br />
       <h3>Présentation publi.codes</h3>
-      Ce site web présente des exemples d'algorithmes encodés avec{" "}
+      Ce site web présente des exemples d&apos;algorithmes encodés avec{" "}
       <a href="https://publi.codes" target="_blank" rel="noopener noreferrer">
         public.codes
       </a>
