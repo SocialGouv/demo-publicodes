9e2efe0fe226495f21aee679f7658f3b8a078d13
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-11-25 02:39:38 +0100

    docs

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -5,9 +5,24 @@ export default function Web() {
       <h3>Présentation publi.codes</h3>
       Ce site web présente des exemples d&apos;algorithmes encodés avec{" "}
       <a href="https://publi.codes" target="_blank" rel="noopener noreferrer">
-        public.codes
+        publi.codes
       </a>
       .
+      <br />
+      <br />
+      Publicodes est un langage déclaratif pour modéliser des domaines métiers
+      complexes en les décomposant en règles élémentaires simples.
+      <br />
+      <br />
+      Les algorithmes ainsi définis peuvent ensuite être exploités dans des
+      applications web, mobiles, API... et être distribués via NPM ou GIT.
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <br />
       <br />
       <br />
