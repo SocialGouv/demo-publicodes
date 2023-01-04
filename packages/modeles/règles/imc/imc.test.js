10136058abedb189675325ae165472ff7a9b2e7a
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2023-01-04 12:38:49 +0100

    fix: fix imc

--- packages/modeles/règles/imc/imc.test.js
+++ packages/modeles/règles/imc/imc.test.js
@@ -1,8 +1,6 @@
 /* eslint-env node18 */
-import fs from "fs";
 import yaml from "js-yaml";
-import path from "path";
-import Engine from "publicodes";
+import Engine, { formatValue } from "publicodes";
 
 import modeles from "@socialgouv/publicodes-demo-modeles";
 const rules = yaml.load(modeles.imc.yaml);
@@ -41,9 +39,46 @@ const tests = [
 test.each(tests)(".imc($poids, $taille)", ({ poids, taille }) => {
   const engine = new Engine(rules);
   engine.setSituation({
-    "patient . poids": poids,
-    "patient . taille": taille,
+    poids: poids,
+    taille: taille,
   });
   const evaluated = engine.evaluate("résultat");
   expect(evaluated.nodeValue).toMatchSnapshot();
+});
+
+test("doit renvoyer le bon IMC pour T=170,P=80)", () => {
+  const engine = new Engine(rules);
+  engine.setSituation({
+    poids: "80kg",
+    taille: "170cm",
+  });
+  const evaluated = engine.evaluate("imc");
+  // teste la valeur brute
+  expect(evaluated.nodeValue).toEqual(27.68166089965398);
+  // teste la valeur formatée
+  expect(formatValue(evaluated)).toEqual("27,68\u00A0kg / m²");
+});
+
+test("doit renvoyer la bonne interpretation pour T=180,P=80)", () => {
+  const engine = new Engine(rules);
+  engine.setSituation({
+    poids: "80kg",
+    taille: "180cm",
+  });
+  const evaluated = engine.evaluate("résultat");
+  expect(evaluated.nodeValue.trim())
+    .toEqual(`Votre IMC est de 24,69\u00A0kg / m²
+Interprétation: poids normal`);
+});
+
+test("doit renvoyer la bonne interpretation pour T=150,P=90)", () => {
+  const engine = new Engine(rules);
+  engine.setSituation({
+    poids: "90kg",
+    taille: "150cm",
+  });
+  const evaluated = engine.evaluate("résultat");
+  expect(evaluated.nodeValue.trim()).toEqual(
+    `Votre IMC est de 40\u00A0kg / m²\nInterprétation: obésité morbide ou massive`
+  );
 });
